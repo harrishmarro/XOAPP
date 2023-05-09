ee5ea8260d1b36e9ec5c7a3d75f04914065e0f47
harrishmarro <harishmacking11@gmail.com>
2023-05-09 23:39:02 +0530

    XOAPP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Board from './Components/Board'
+import Board1 from './Components/Board1'
+
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      <Board1/>
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
