248fb78fef6e4bb57cc0a996ad1f7a525fdb8bde
Harish PS <74762783+harrishmarro@users.noreply.github.com>
2023-05-09 23:52:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Board from './Components/Board'
+import Board1 from './Components/Board1'
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='page'>
+      <Board1/>
+    </div>
+    
+  )
+}
+
+export default App
